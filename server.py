3c68989a0ef94560e1c04828739d3ce6f6b7d40a
Roger <rogerlais@gmail.com>
2021-06-06 09:33:53 -0300

    100% documentado

--- server.py
+++ server.py
@@ -26,51 +26,44 @@ class ThreadedServer(Thread):
         self.listen()
 
     def listen(self):
-        # create an instance of socket
+        # cria instancia do socket
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-        # bind the socket to its host and port
+        # host e porta - sem teste para multihomed
         self.sock.bind((self.host, self.port))
         if self.debug:
             print(datetime.now())
             print('SERVIDOR socket ativo', self.host, self.port, '\n')
 
-        # start listening for a client
+        # inicia a escuta
         self.sock.listen(5)
         if self.debug:
             print(datetime.now())
             print('SERVIDOR aguardando clientes...', '\n')
         while True:
-            # get the client object and address
+            # leotura dos dados do socket
             client, address = self.sock.accept()
 
-            # set a timeout
+            # chamada para timeout
+            #todo fazer callback melhor
             client.settimeout(self.timeout)
 
             if self.debug:
                 print(datetime.now())
                 print('CLIENTE Connectado:', client, '\n')
 
-            # start a thread to listen to the client
+            # Inicia thread com o socket suspenso
             Thread(
                 target=self.listenToClient,
                 args=(client, address, self.callback)
             ).start()
 
-            # send the client a connection message
-            # res = {
-            #     'cmd': 'connected',
-            # }
-            # response = dumps(res)
-            # client.send(response.encode('utf-8'))
-
     def listenToClient(self, client, address, callback):
-        # set a buffer size ( could be 2048 or 4096 / power of 2 )
         size = 1024
         while True:
             try:
-                # try to receive data from the client
+                # leitura dos dados do cliente
                 data = client.recv(size).decode('utf-8')
                 if data:
                     json = loads(data.rstrip('\0'))
@@ -82,7 +75,7 @@ class ThreadedServer(Thread):
                         print('\n')
 
                     if callback is not None:
-                        callback(self, client, address, json)
+                        callback(self, client, address, json) #! calback com o Dict()/JSON
 
                 else:
                     raise 'Client disconnected'
@@ -98,7 +91,7 @@ class ThreadedServer(Thread):
 #exemplo
 def some_callback(server, sockClient, address, data):
     print('Dado recebido', data)
-    # send a response back to the client
+    # enviar resposta ao cliente
     try:
         hostRequest = DataWrapper.loadFromJSON( data )
         response = loads(hostRequest.json)
@@ -108,5 +101,6 @@ def some_callback(server, sockClient, address, data):
 
 
 
-if __name__ == "__main__":
-    ThreadedServer('127.0.0.1', 8008, timeout=86400, callback=some_callback, debug=True).start()
+#!usar modulo apenas
+#if __name__ == "__main__":
+#    ThreadedServer('127.0.0.1', 8008, timeout=86400, callback=some_callback, debug=True).start()
